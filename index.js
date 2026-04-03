8900039b162d29bcc02581b4885a3da7d13645a8
Brodorovna <75303309+Brodorovna@users.noreply.github.com>
2026-04-03 16:06:26 +0300

    Update and rename index.html to index.js

--- index.js
+++ index.js
@@ -4,6 +4,35 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Интерактивная карта</title>
+<script src="dist/oi.hexmap.js"></script>
+  <style>/* Define the size of the hexmap area */
+#hexmap1 { height: 300px; width: 100%; }
+    </style>
+  // Attach our hexmap to <div id="hexmap1"></div>
+hex = new OI.hexmap(document.getElementById('hexmap1'),{
+	// Define the HexJSON - https://open-innovations.org/projects/hexmaps/hexjson
+	'hexjson':{
+		"layout":"odd-r",
+		"hexes": {
+			"A":{"n":"0,1","q":0,"r":1},
+			"B":{"n":"1,1","q":1,"r":1},
+			"C":{"n":"2,1","q":2,"r":1},
+			"D":{"n":"0,2","q":0,"r":2},
+			"E":{"n":"1,2","q":1,"r":2},
+			"F":{"n":"2,2","q":2,"r":2}
+		}
+	}
+});
+<div id="hexmap1"></div>
+</body>
+</html>
+
+/**<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Интерактивная карта</title>
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
   <style>
     body {
@@ -39,3 +68,4 @@
   </script>
 </body>
 </html>
+**/
